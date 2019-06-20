7715b357c5fd166a47195d81c142c89c8eeef00c
FunGamesLeaks <mcdtloeser@gmail.com>
2019-06-20 18:23:41 +0200

    init

---
+++ src/oodle/OodleCompressionLevel.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package oodle;
+
+/**
+ * @author FunGames
+ *
+ */
+public class OodleCompressionLevel {
+	public static final int COMPRESSION_LEVEL_NONE = 0;
+	public static final int COMPRESSION_LEVEL_SUPER_FAST = 1;
+	public static final int COMPRESSION_LEVEL_VERY_FAST = 2;
+	public static final int COMPRESSION_LEVEL_FAST = 3;
+	public static final int COMPRESSION_LEVEL_NORMAL = 4;
+	public static final int COMPRESSION_LEVEL_OPTIMAL1 = 5;
+	public static final int COMPRESSION_LEVEL_OPTIMAL2 = 6;
+	public static final int COMPRESSION_LEVEL_OPTIMAL3 = 7;
+	public static final int COMPRESSION_LEVEL_OPTIMAL4 = 8;
+	public static final int COMPRESSION_LEVEL_OPTIMAL5 = 9;
+}
---
+++ src/test/Test.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package test;
+
+/**
+ * @author FunGames
+ *
+ */
+public class Test {
+	public static void main(String[] args) {
+		
+	}
+}
